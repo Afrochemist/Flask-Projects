f1073c8aeef8cbc1cd5f67b5cc992887084021d3
Afrochemist <yamaha366@aol.com>
2019-11-03 11:47:01 -0600

    Need to fix simplle bug

---
+++ samplesite/adoption.py
@@ -0,0 +1,77 @@
+import os
+from forms import AddForm, DelForm
+from flask import Flask, render_template, url_for,redirect
+from flask_sqlalchemy import SQLAlchemy
+from flask_migrate import Migrate
+
+app = Flask(__name__)
+
+app.config['SECRET_KEY'] = 'mysecretkey'
+
+######SQL Database Section######
+
+basedir = os.path.abspath(os.path.dirname(__file__))
+app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///'+os.path.join(basedir,'data.sqlite')
+app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
+
+db = SQLAlchemy(app)
+Migrate(app,db)
+
+
+#### Models ####
+
+class Puppy(db.Model):
+
+    __tablename__ = 'puppies'
+
+    id = db.Column(db.Integer, primary_key=True)
+    name = db.Column(db.Text)
+
+    def __init__(self,name):
+        self.name = name
+
+    def __repr__(self):
+        return f'Puppy name: {self.name}'
+
+
+#### View fUNCTIONS - Have Forms ####
+@app.route('/')
+def index():
+    return render_template('home.html')
+
+
+@app.route('/add',methods=['GET','POST'])
+def add_pup():
+
+    form = AddForm()
+
+    if form.validate_on_submit():
+
+        name = form.name.data
+        new_pup = Puppy(name)
+        db.session.add(new_pup)
+        db.session.commit()
+
+        return redirect(url_for('list'))
+
+    return render_template('add.html',form=form)
+
+@app.route('/list')
+def pup():
+    puppies = Puppy.query.all()
+    return render_template('list.html', puppies=puppies)
+
+@app.route('/delete', methods=['GET','POST'])
+def del_pup():
+    if validate_on_submit():
+        id = form.id.data
+        pup = Puppy.query.get(id)
+        db.session.delete(pup)
+        db.session.commit()
+
+        return redirect(url_for('list'))
+
+    return render_template('delete.html',form=form)
+
+if __name__ == '__main__':
+    app.run(debug=True)
